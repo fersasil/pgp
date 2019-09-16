021e859c5f9401526fb4e3985d1ce317e0c83cf9
Ikkisoad <henrique.alberto.andrade@hotmail.com>
2019-09-16 15:55:55 -0300

    Uma Mensagem

---
+++ backend/helpers/dbHelper.js
@@ -0,0 +1,38 @@
+const dbHelper = require('../helpers/dbFunctions');
+// const dbFunc = new dbHelper('usuario');
+
+module.exports = class {
+    constructor(params) {
+        this.name = params.name;
+        this.password = params.password;
+        this.email = params.email;
+        this.id = params.id;
+    }
+
+    async query(query, args) {
+        return new Promise(async(resolve, reject) => {
+            const queryVar = args;
+            const queryStr = query;
+            let conn;
+
+            try {
+                conn = await pool.getConnection();
+                const rows = await conn.query(queryStr, queryVar);
+                conn.end();
+                resolve(rows);
+            } catch (err) {
+                conn.end();
+                if (reject) reject(err);
+                errorHandler(err);
+            }
+        });
+    }
+
+
+    async findByEmail(email) {
+        const commonInfo = this;
+
+        return this.query("SELECjhf" + email)
+    }
+
+};
--- backend/test.js
+++ backend/test.js
@@ -1,23 +1,33 @@
-const pool = require('./helpers/dbConnect');
+// const pool = require('./helpers/dbConnect');
 
 // const queryVar = args;
-const queryStr = "";
-let conn;
+const queryStr = "SELECT * FROM `Event` INNER JOIN `Address` ON `Event`.`Address_idAddress` = `Address`.`idAddress` WHERE 1";
+// let conn;
 
-async function teste(){
+// async function teste(){
 
-    try {
-        conn = await pool.getConnection();
-        const rows = await conn.query(queryStr, queryVar);
-        console.log(rows);
-        
-        conn.end();
-    } catch (err) {
-        conn.end();
-        // errorHandler(err);
-    }
+//     try {
+//         console.log("OLA 1")
+//         conn = await pool.getConnection();
+//         const rows = await conn.query(queryStr);
+//         console.log(...rows);
 
-}
+//         conn.end();
+//     } catch (err) {
+//         console.log(err)
+
+//         conn.end();
+//         // errorHandler(err);
+//     }
+
+// }
 
 
-teste();
+// teste();
+
+
+const dbHelper = require("./helpers/dbHelper");
+const dbFunc = new dbHelper('Event');
+
+
+dbFunc.findByEmail('jotge');
